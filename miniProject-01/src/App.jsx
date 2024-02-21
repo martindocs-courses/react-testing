bcdafda817e2ffc4bb15b7fc077be076cd785373
Marcin Tatarski <martin.tatarski>
2024-02-21 19:27:14 +0000

    Add test for User Form

--- miniProject-01/src/App.jsx
+++ miniProject-01/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
+import { useState } from 'react';
 
 function App() {
+  const[userList, setUserList] = useState([])
+
+  const onUserAdd = (user) => {
+    setUserList([...userList, user]);
+  }
 
   return (
-    <h2>Hello Tests!</h2>
+    <>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList userList={userList} />
+    </>
   )
 }
 
-export default App
+export default App;
